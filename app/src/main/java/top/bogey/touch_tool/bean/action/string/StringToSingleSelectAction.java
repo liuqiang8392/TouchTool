f566432860cbac98faad297cd35d4c0fabb96be5
mr-bogey <1762076107@qq.com>
2026-03-18 17:10:47 +0800

    feat(action): 添加字符串转单选动作的选项配置功能

--- app/src/main/java/top/bogey/touch_tool/bean/action/string/StringToSingleSelectAction.java
+++ app/src/main/java/top/bogey/touch_tool/bean/action/string/StringToSingleSelectAction.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import top.bogey.touch_tool.R;
 import top.bogey.touch_tool.bean.action.ActionType;
@@ -11,37 +12,59 @@ import top.bogey.touch_tool.bean.action.parent.CalculateAction;
 import top.bogey.touch_tool.bean.pin.Pin;
 import top.bogey.touch_tool.bean.pin.pin_objects.PinBase;
 import top.bogey.touch_tool.bean.pin.pin_objects.PinObject;
+import top.bogey.touch_tool.bean.pin.pin_objects.pin_list.PinList;
 import top.bogey.touch_tool.bean.pin.pin_objects.pin_string.PinSingleSelect;
 import top.bogey.touch_tool.bean.pin.pin_objects.pin_string.PinString;
 import top.bogey.touch_tool.service.TaskRunnable;
 
 public class StringToSingleSelectAction extends CalculateAction {
+    private final transient Pin optionsPin = new Pin(new PinList(new PinString()), R.string.string_to_single_select_action_options);
     private final transient Pin textPin = new Pin(new PinString(), R.string.string_to_single_select_action_select);
     private final transient Pin singleSelectPin = new Pin(new PinSingleSelect(), R.string.pin_string_select, true);
 
     public StringToSingleSelectAction() {
         super(ActionType.STRING_TO_SINGLE_SELECT);
-        addPins(textPin, singleSelectPin);
+        addPins(optionsPin, textPin, singleSelectPin);
     }
 
     public StringToSingleSelectAction(JsonObject jsonObject) {
         super(jsonObject);
-        reAddPins(textPin, singleSelectPin);
+        reAddPins(optionsPin, textPin, singleSelectPin);
     }
 
     @Override
     public void calculate(TaskRunnable runnable, Pin pin) {
+        PinList options = getPinValue(runnable, optionsPin);
         PinObject text = getPinValue(runnable, textPin);
-        PinSingleSelect singleSelect = singleSelectPin.getValue();
+        PinSingleSelect select = singleSelectPin.getValue();
+
         Pin linkedPin = singleSelectPin.getLinkedPin(runnable.getTask());
-        if (linkedPin == null) return;
         PinBase value = linkedPin.getValue();
+
         if (value instanceof PinSingleSelect pinSingleSelect) {
-            singleSelect.setOptions(pinSingleSelect.getOptions());
-            singleSelect.setValue(text.toString());
+            if (pinSingleSelect.isDynamic()) {
+                if (options.isEmpty()) {
+                    select.setOptions(new ArrayList<>(Collections.singletonList(text.toString())));
+                    select.setValue(text.toString());
+                } else {
+                    List<String> optionsList = new ArrayList<>();
+                    for (PinObject option : options) {
+                        optionsList.add(option.toString());
+                    }
+                    select.setOptions(optionsList);
+                    select.setValue(text.toString());
+                }
+            } else {
+                select.setOptions(pinSingleSelect.getOptions());
+                if (pinSingleSelect.getOptions().contains(text.toString())) {
+                    select.setValue(text.toString());
+                } else {
+                    select.setValue(pinSingleSelect.getValue());
+                }
+            }
         } else if (value instanceof PinString) {
-            singleSelect.setOptions(new ArrayList<>(Collections.singletonList(text.toString())));
-            singleSelect.setValue(text.toString());
+            select.setOptions(new ArrayList<>(Collections.singletonList(text.toString())));
+            select.setValue(text.toString());
         }
     }
 }
